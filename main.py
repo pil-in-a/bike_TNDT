b206bea06a452e7e4673c0a1681c89631ce635ec
pilina <pil.in.a@centrum.cz>
2024-07-14 20:57:45 +0200

    - drobný refactoring: Camera jako class, přejmenování proměnných - přidání ukládání dat do matfilu

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from PyQt6 import QtCore
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QMainWindow
 from commands import iray_commands as i_c  # dictionary s hex příkazama
+from scipy.io import savemat # pro ukládání matláku
 # další dependency zde je PyQt6 - pip install pyqt6 (pro linux)
 
 # -----------------
@@ -22,28 +23,34 @@ class CustomMainWindow(QMainWindow):
         if event.key() == Qt.Key.Key_Q:  # corrected to `Qt.Key.Key_Q`
             self.close()  # Close the window
 
+class Camera:
+    # třída Camera, při iniciaci provedfe cv.Videocapture()
+    # obsahuje už nějaký funkce, který se s ní prováděj
+    def __init__(self, ser, device_index):
+        self.ser = ser
+        self.device = cv.VideoCapture(device_index, cv.CAP_V4L2)
 
-def send_cmd(prikaz):
-    # převod hex stringu na byt
-    prikaz = i_c[prikaz]
-    prikaz_hex = bytes.fromhex(prikaz)
-    ser.write(prikaz_hex)
-    # tady by to chtělo zase zavřít port
-    # return žádnej nepotřebuju I guess
+    def send_command(self, command):
+        ser.write(bytes.fromhex(i_c[command]))
+
+    def read_frame(self):
+        return self.device.read()
+
+    def setup_raw_mode(self):
+        self.device.set(cv.CAP_PROP_FOURCC, cv.VideoWriter.fourcc('Y', '1', '6', ''))
+        self.device.set(cv.CAP_PROP_CONVERT_RGB, 0)
+
+    def release_camera(self):
+        self.device.release()
 
 
-def kamera_raw_nastaveni(kamera):
-    kamera.set(cv.CAP_PROP_FOURCC, cv.VideoWriter.fourcc('Y', '1', '6', ''))
-    kamera.set(cv.CAP_PROP_CONVERT_RGB, 0.0)  # opencv to nepřevádí na RGB -> 540, 512, 2
+def create_thumbnail(camera):
+    _, image = camera.read_frame()
+    return image
 
 
-def vytvor_thumbnail(kamera):
-    kamera_jede, obrazek = kamera.read()
-    return obrazek
-
-
-def vytvor_filename(zacatek, konec, n):
-    fps = str(n / (konec - zacatek))
+def create_filename(start, stop, n):
+    fps = str(n / (stop - start))
     filename = f'{time.strftime("%H%M%S")}FPS{fps[0:2]}_{fps[3:6]}'
     return filename, fps
 
@@ -60,18 +67,16 @@ y_watch = 256
 ser = serial.Serial(f'/dev/ttyUSB{port}')
 ser.baudrate = 115200
 
-time.sleep(2)  # dvě sekundy aby se všechno vzpamatovalo - hlavně serial
+camera = Camera(ser, device_index)
 
 # OVLADANI KAMERY po serial portu
-#send_cmd('Auto NUC off')
-#send_cmd('NUC - Shutter')
-send_cmd('DVI - BT.1120')
-send_cmd('PLT - Lava')
-send_cmd('IF - horizontal')
-# TODO: tady to cvaká 2x ... tak se podívat, co to dělá
+# kdoví proč to tu zároven i cvakne :D
+camera.send_command('DVI - BT.1120')
+camera.send_command('PLT - Lava')
+camera.send_command('IF - horizontal')
 
 # otevření opencv streamu pomocí V4L2 (jine moznosti jsou treba DIVX apod.)
-iray = cv.VideoCapture(device_index, cv.CAP_V4L2)
+# iray = cv.VideoCapture(device_index, cv.CAP_V4L2)
 
 # -----------------
 # LIVE VIEW
@@ -83,13 +88,13 @@ cv.resizeWindow('live-view', 1200, 1000)
 # loop zobrazující liveview
 while True:
     # Capture frame-by-frame
-    pohoda, frame_live = iray.read()
+    pohoda, frame_live = camera.read_frame()
     if not pohoda:  # ret je return boolean, True když v pohodě vysílá
         break
 
     # Display the resulting frame
-    resized_obrazek = cv.resize(frame_live, (int(640*1.9), int(512*1.9)))
-    cv.imshow('live-view', resized_obrazek)
+    frame_live = cv.resize(frame_live, (int(640*1.9), int(512*1.9)))
+    cv.imshow('live-view', frame_live)
     key = cv.waitKey(1)
     if key == ord('q'):  # exit on q ... if key == 27 je pro ESC
         break
@@ -97,13 +102,13 @@ while True:
 cv.destroyAllWindows()
 
 # po zaostření a před záznamem (tam už mám nastavenou úpravu RAW formátu na Y16)
-thumbnail = vytvor_thumbnail(iray)
+thumbnail = create_thumbnail(camera)
 
 # -------------------
 # ZAZNAM
 # ----------------------
 # nastavení správného raw formátu
-kamera_raw_nastaveni(iray)
+camera.setup_raw_mode()
 
 # vypocet intervalu pro QtTimer
 cas_ms = 1000 // set_fps  # 1000 ms děleno (// pro integer) fpskama
@@ -148,7 +153,7 @@ data, casy, data_bod = [], [], []
 
 def update():
     # funkce updatující data pro zobrazení a záznam
-    ret, frame = iray.read() # shape je (512, 640, 2)
+    success, frame = camera.read_frame() # shape je (512, 640, 2)
 
     frame = frame.astype('int16')  # vic mista
     frame = frame[:,:,0] + (frame[:,:,1] << 8) # prvni frame + druhej * 256
@@ -166,10 +171,10 @@ def update():
 
 
 # Shutter před náběrem
-send_cmd('DVI - LVCMOS')
-send_cmd('DVS - NUC')
-send_cmd('Auto NUC off')
-send_cmd('NUC - Shutter')
+camera.send_command('DVI - LVCMOS')
+camera.send_command('DVS - NUC')
+camera.send_command('Auto NUC off')
+camera.send_command('NUC - Shutter')
 time.sleep(2)  # dvě sekundy prodleva aby shutter nezasáhnul do záznamu
 
 # spouštění funkce update
@@ -180,7 +185,7 @@ timer.start(cas_ms)
 # spuštění celýho Qt molochu
 app.exec()
 
-iray.release()  # Release the camera
+camera.release_camera()  # Release the camera
 # ------------------------
 # ZPRACOVÁNÍ A UKLÁDÁNÍ DAT
 # --------------------------
@@ -188,13 +193,13 @@ print('Probíhá ukládání dat')
 # data dostavam z funkce jako list of 3D arrays (512,640) a dělám z toho (N, 512,640) int16
 data = np.stack(data, axis=0, dtype='int16')
 
-folder_name, real_fps = vytvor_filename(casy[0], casy[-1], data.shape[0])
+folder_name, real_fps = create_filename(casy[0], casy[-1], data.shape[0])
 # vytvoření folderu s názvem čas měření, FPS
 makedirs(folder_name, exist_ok=True)
 
 # uložení dat
 np.save(file=f'{folder_name}/data.npy', arr=data)
-print(f'Byl vytvořen folder s názvem {folder_name[:-1]}. Data mají {data.shape[0]} snímků a {real_fps[0:6]} FPS.')
+print(f'Byl vytvořen folder s názvem {folder_name}. Data mají {data.shape[0]} snímků a {real_fps[0:6]} FPS.')
 
 # uložení timestampu
 with open(f'{folder_name}/timestamps.csv', 'w', newline='') as csvfile:
@@ -204,8 +209,9 @@ with open(f'{folder_name}/timestamps.csv', 'w', newline='') as csvfile:
 # uložení thumbnailu
 cv.imwrite(f"{folder_name}/thumb.png", thumbnail)
 
-# vyslání příkazu na NUC shutter
-send_cmd('NUC - Shutter')
+# uložení matlabu
+savemat(f'{folder_name}/{folder_name}.mat', {f'{folder_name}': data})
 
-time.sleep(2)  # dvě sekundy prodleva aby shutter nezasáhnul do záznamu
-send_cmd('Factory defaults')
+# vyslání příkazu na NUC shutter
+camera.send_command('NUC - Shutter')
+camera.send_command('Factory defaults')
